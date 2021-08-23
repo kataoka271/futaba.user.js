95eadbae37558a79853f463b6ee9e527dae12c17
Hiroaki Kataoka <k_hir@hotmail.com>
2021-08-23 22:22:10 +0900

    do not change auto scroll status when not running

--- src/Futaba.user.ts
+++ src/Futaba.user.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Futaba
 // @namespace    https://github.com/kataoka271
-// @version      0.0.10
+// @version      0.0.11
 // @description  Futaba
 // @author       k_hir@hotmail.com
 // @match        https://may.2chan.net/b/*
@@ -609,7 +609,7 @@
         } else if (e.key === "V" && autoScr.dy / 2 >= 1) {
           autoScr.dy /= 2;
           autoScr.status(`scroll volume down: tm=${autoScr.tm} dy=${autoScr.dy} dy/tm=${(autoScr.dy / autoScr.tm) * 1000}`);
-        } else if (e.key === "s") {
+        } else if (e.key === "s" && autoScr.running) {
           if (!autoScr.paused) {
             autoScr.pause();
             autoScr.status("auto-scroll paused", true);
