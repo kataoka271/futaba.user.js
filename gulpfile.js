ada41ea8c5139dc9aeb1d16ba4d2bf7a32561a3f
Hiroaki Kataoka <k_hir@hotmail.com>
2022-02-05 18:06:58 +0900

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,19 @@
-const { src, dest, watch, series } = require("gulp");
+const { dest, watch, series } = require("gulp");
 const ts = require("gulp-typescript");
 const fileinclude = require("gulp-file-include");
 const lec = require("gulp-line-ending-corrector");
 
 const tsProject = ts.createProject("tsconfig.json");
 
-function defaultTask() {
-  return tsProject.src()
+exports.default = function () {
+  return tsProject
+    .src()
     .pipe(fileinclude())
     .pipe(tsProject())
-    .js
-    .pipe(lec({ eolc: "CRLF" }))
+    .js.pipe(lec({ eolc: "CRLF" }))
     .pipe(dest("dist"));
-}
+};
 
-function watchTask() {
-  watch("src/**/*.ts", series("default"));
-}
-
-exports.default = defaultTask;
-exports.watch = watchTask;
+exports.watch = function () {
+  watch(["src/**/*.ts", "src/**/*.css"], series("default"));
+};
