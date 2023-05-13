d1e79862b735b2c05f7a5f0f18752437edf92e3e
Hiroaki Kataoka <k_hir@hotmail.com>
2023-05-14 01:06:27 +0900

    extract export.default to build function

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
-const { dest, watch, series } = require("gulp");
+const { dest, watch } = require("gulp");
 const ts = require("gulp-typescript");
 const fileinclude = require("gulp-file-include");
 const lec = require("gulp-line-ending-corrector");
 
 const tsProject = ts.createProject("tsconfig.json");
 
-exports.default = function () {
+function build() {
   return tsProject
     .src()
     .pipe(fileinclude())
     .pipe(tsProject())
     .js.pipe(lec({ eolc: "CRLF" }))
     .pipe(dest("dist"));
-};
+}
+
+exports.default = build;
 
 exports.watch = function () {
-  watch(["src/**/*.ts", "src/**/*.css"], series("default"));
+  watch(["src/**/*.ts", "src/**/*.css"], build);
 };
